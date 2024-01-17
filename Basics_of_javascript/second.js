d9513059e503392e963952d7cc1abff1d2fc39db
Asta kk <156662799+Astakk123@users.noreply.github.com>
2024-01-17 10:23:37 +0000

    This is second class on variables let, var, const,  not done yet

---
+++ Basics_of_javascript/second.js
@@ -0,0 +1,7 @@
+// here in this class I started with the variables and some more concepts on the varibales like when we write the keywords why do we use the capital letter people say different things and try to explain in their way but i learned tha it is earear to read.
+const accountId = 12365
+// here you the const is used when we don't change its value it is constant like id we will do some investigation on it 
+let accountEmail = "As@gmail.com"
+var accountPassword = "123221"
+accountCity = "Bhubaneswar"
+console.log (accountId)
