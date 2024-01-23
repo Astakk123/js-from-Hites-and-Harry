9228d9f23d904bb6db0d55095a3a4bf576640cf7
Asta kk <156662799+Astakk123@users.noreply.github.com>
2024-01-23 10:12:34 +0000

    Comparison of datat

---
+++ Basics_of_javascript/seventh.js
@@ -0,0 +1,34 @@
+// datatype Summary
+//primitive
+//7types : String, Number, Boolean, null, undefined, Symbol, BigInt
+
+const score = 100;
+const scoreValue = 100.4;
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail;
+const Id = Symbol("123")
+const anotherId = Symbol("123");
+console.log(Id === anotherId);
+
+//REference (Non primitive)
+
+// Array, Object, Funtion
+
+
+
+const heros = ["shaktiman", "naagraj", "doga"]; //Arry
+
+// this is object ex-
+ let: myObj ={  
+    name: "Asta",
+    age: 23,
+
+ }
+
+ // this is how we use funtion data type
+ 
+ const myFunction = function(){
+    console.log("Hello world");
+ }
+
