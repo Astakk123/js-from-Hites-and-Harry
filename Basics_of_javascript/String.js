2fdc6d08f58eac9e2c736b5697eff768ba4a0180
Asta kk <156662799+Astakk123@users.noreply.github.com>
2024-01-24 06:29:20 +0000

    Strings and how to use it

---
+++ Basics_of_javascript/String.js
@@ -0,0 +1,13 @@
+// all about strings   ===================================================== class 11 ==========
+  
+const name = "Asta"
+const repoCount = 33
+
+//console.log(name + repoCount + " value")  this is little outdated so people don't use this syntax 
+
+// we use backticks and we write this way this syntax is more readable and usefull called string interpolletion it make placehoder
+console.log(`hello my name is ${name} and my repo count is ${repoCount}`)
+
+const gameName = new String("Astakk")
+
+console.log(gameName[0]) // giving keyvalue pair we can see the result is "A"
