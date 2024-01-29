c47271ef16501e65f4052a8f07149ff7716e9442
Asta kk <156662799+Astakk123@users.noreply.github.com>
2024-01-29 03:42:25 +0000

    String in detail

--- Basics_of_javascript/String.js
+++ Basics_of_javascript/String.js
@@ -6,6 +6,7 @@ const repoCount = 33
 //console.log(name + repoCount + " value")  this is little outdated so people don't use this syntax 
 
 // we use backticks and we write this way this syntax is more readable and usefull called string interpolletion it make placehoder
+
 console.log(`hello my name is ${name} and my repo count is ${repoCount}`)
 
 const gameName = new String("Astakk") //another way to declare string
