497176ea205a1c7ad0e8b11f6e7cc02f96045c38
Asta kk <156662799+Astakk123@users.noreply.github.com>
2024-02-02 04:54:24 +0000

    javascript execution and call stack

---
+++ javamediocore/class25.js
@@ -0,0 +1,10 @@
+// ++++++++++++++++++++++++ How does javascript execute code + call stack ++++++++++++++++++++++++++++++++
+
+//javascript is a single threaded
+//jvascript Execution context 
+
+// there are types of execution context like i.e (1) Global Execution context (2) Function Execution context (3) Eval Execution Context
+
+// all the process happen in phases i.e
+                                //(1) creation phase or memory creation phase
+                                //(2) Execution Phase
