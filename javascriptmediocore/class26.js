fc5647703bd300e0ac91ea8e6a1d0a837da1c7be
Asta kk <156662799+Astakk123@users.noreply.github.com>
2024-02-03 06:23:35 +0000

    control flow in javascript

---
+++ javascriptmediocore/class26.js
@@ -0,0 +1,36 @@
+// control flow i.e control flow is used to control the execution of the code mens when to run and which code is run these all are done by using some control flow like condition see below 
+
+// if (condition) {
+
+// }
+
+// if the condition is true then only the code will execute otherwise not ex-
+// if (userLoggedIn){
+
+// } 
+//her if the user is loggedIn then it is true so it will run otherwise it will not run 
+// there are number of operator to compare to apply condition like
+/* 
+< lessthan 
+> greaterthan
+<= lesstahn or equality
+>= greaterthan or equality
+= this is just on operator to assign the value to the variables
+== equality
+!= not equal 
+=== strict equal (means it will check the typeof data also strictly equal or not)
+!== strict not eqqual 
+*/
+const temp = 21
+
+if ( temp  < 50){
+    console.log("yes")
+}
+// console.log("no");// this code has nothing to do with what condition is given above because it is global scope not in the block scope so what should we do? see here 
+
+
+
+
+
+
+
